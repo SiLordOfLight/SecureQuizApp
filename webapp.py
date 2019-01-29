ef159eca067bfe51ffcb9342438d987b173dab66
SiLordOfLight <jatnerd@gmail.com>
2019-01-29 10:23:46 -0800

    1/29 @ 10:23a

--- webapp.py
+++ webapp.py
@@ -2,18 +2,44 @@ import os
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
+import json
 
 app = Flask(__name__)
 
 
-app.secret_key=os.environ["SECRET_KEY"];
+app.secret_key=os.environ["SECRET_KEY"]
 
+numberOfQuestions = os.environ["NUM_QS"]
+
+questions = []
+
+for i in range(1,numberOfQuestions+1):
+    inStr = os.environ["Q"+i]
+
+    questions.append(json.loads(inStr))
+
+
+# {"question":"What is Jake's favorite color?", "A":"Red", "B":"Green", "C":"Gold", "D":"Silver", "correct":"D"}
 
 
 @app.route("/")
-def render_main:
+def render_main():
     return render_template('home.html')
 
-@app.route('/page1')
-def renderPage1:
-    return render_template('page1.html')
+@app.route('/question', methods=['GET','POST'])
+def renderQuestionPage():
+    questionNum = request.args["qnum"]
+
+    question = questions[questionNum-1]
+
+    answers = [question['A'], question['B'], question['C'], question['D']]
+
+    last = True if questionNum == numberOfQuestions else False
+
+    return render_template('question.html', qnum=questionNum, question=question["question"], answers=answers, last=last)
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
