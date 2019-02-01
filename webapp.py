2a0a50dbb2f6f8e920c88ece4beff07215c2ada1
SiLordOfLight <jatnerd@gmail.com>
2019-02-01 10:10:05 -0800

    Final update

--- webapp.py
+++ webapp.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 
 app.secret_key=os.environ["SECRET_KEY"]
 
-numberOfQuestions = os.environ["NUM_QS"]
+numberOfQuestions = int(os.environ["NUM_QS"])
 
 questions = []
 
 for i in range(1,numberOfQuestions+1):
-    inStr = os.environ["Q"+i]
+    inStr = os.environ["Q%i" % i]
 
     questions.append(json.loads(inStr))
 
@@ -24,19 +24,39 @@ for i in range(1,numberOfQuestions+1):
 
 @app.route("/")
 def render_main():
+    session["score"] = 0
+
     return render_template('home.html')
 
 @app.route('/question', methods=['GET','POST'])
 def renderQuestionPage():
-    questionNum = request.args["qnum"]
+    if "answer" in request.form:
+        oldQuestionNum = int(request.args["qnum"]) -1
+        oldQ = questions[oldQuestionNum-1]
 
+        if oldQ["correct"] == request.form["answer"]:
+            session["score"] = session["score"] + 1
+
+        print("Question #%i: Correct answer: %s || Inputted answer: %s" % (oldQuestionNum, oldQ["correct"], request.form["answer"]))
+
+    questionNum = int(request.args["qnum"])
     question = questions[questionNum-1]
 
     answers = [question['A'], question['B'], question['C'], question['D']]
 
     last = True if questionNum == numberOfQuestions else False
-
     return render_template('question.html', qnum=questionNum, question=question["question"], answers=answers, last=last)
+
+@app.route("/finPage", methods=['GET', 'POST'])
+def render_finPage():
+    if "answer" in request.form:
+        oldQuestionNum = numberOfQuestions
+        oldQ = questions[oldQuestionNum-1]
+
+        if oldQ["correct"] == request.form["answer"]:
+            session["score"] = session["score"] + 1
+
+    return render_template('finPage.html', score = session['score'])
 
 
 
